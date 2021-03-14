b3f71cee8c885bcd7bf988fddeacfec662260ee3
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2021-03-13 23:53:46 -0700

    Fix flipping sprite

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -36,7 +36,7 @@ public class PlayerMovement : PhysicsObject
             }
         }
 
-        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0) : (move.x < 0));
         if (flipSprite)
         {
             this.spriteRenderer.flipX = !this.spriteRenderer.flipX;
